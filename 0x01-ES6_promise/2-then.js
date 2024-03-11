0cf6054aaf8a4a613b5cda6f934e46d40ee8f3a9
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-03-11 17:25:28 +0100

    Modify file export

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,3 @@
-function handelResponseFromAPI(promise) {
+export default function handelResponseFromAPI(promise) {
     promise.then((successMessage) => {return({status: 200, body: success});}).catch((error) => {return({});}).then((error) => { console.log("Got a response from the API"); });
 }
