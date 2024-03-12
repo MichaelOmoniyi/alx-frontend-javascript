08e94f46ed2cfcb4801d9c4689d62ab6e2439ac5
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-03-12 13:17:25 +0100

    Upload solution to task 4

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,14 @@
+export default function signUpUser(firstName, lastName) {
+  const user = {
+    firstName: firstName,
+    lastName: lastName,
+  };
+
+  return new Promise((resolve, reject) => {
+    if (user) {
+      resolve(user);
+    } else {
+      reject(Error());
+    }
+  });
+}
