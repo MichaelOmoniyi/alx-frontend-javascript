ff4123612cf3946efdbe08fb170b6d58b7614c58
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-03-12 13:07:13 +0100

    Modify solution to task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,6 @@
 export default function handelResponseFromAPI(promise) {
-    promise.then((successMessage) => {return({status: 200, body: success});}).catch((error) => {return({});}).then((error) => { console.log("Got a response from the API"); });
+    return promise
+    .then(() => {return({status: 200, body: success})})
+    .catch(() => {return(Error())})
+    .finally(() => { console.log("Got a response from the API")});
 }
