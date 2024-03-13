fc91007fd6e0b22ee5cd9bb2c409a060b89ef65d
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-03-13 05:24:07 +0100

    Update files to be lint compliant

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const resultArray = [];
-  for (let idx of array) {
+  for (const idx of array) {
     resultArray.push(appendString + idx);
   }
 
