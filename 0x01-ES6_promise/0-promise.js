56cb8f03a756ed2f94f5ae99168570861cb9187a
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-03-14 01:03:21 +0000

    Update solution to task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,5 @@
 export default function getResponseFromAPI() {
-<<<<<<< HEAD
     return new Promise((resolve, reject) => {
         resolve(true);
     });
-=======
-  const success = true;
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
->>>>>>> 0c04b82e711f55fbaf06eff3583792005937d60a
 }
