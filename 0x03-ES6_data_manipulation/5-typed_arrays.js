b2d8671f960d2fbfe6bb57b055d7e1650741f138
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-03-18 00:26:51 +0000

    Create a typed array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position === length) {
+    throw new Error('Position outside range');
+  }
+
+  const Buffer = new ArrayBuffer(length);
+  const view = new DataView(Buffer);
+  view.setInt8(position, value);
+  return view;
+}
