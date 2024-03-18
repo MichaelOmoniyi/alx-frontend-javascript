de598c070a5540ceeb5f6a4d16068a72d95ad7e0
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-03-18 00:29:12 +0000

    Creates a string from some set values

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof (startString) !== 'string') {
+    return '';
+  }
+
+  let newString = '';
+
+  set.forEach((elem) => {
+    if (elem && elem.startsWith(startString)) {
+      newString += `${elem.slice(startString.length)}-`;
+    }
+  });
+  return newString;
+}
